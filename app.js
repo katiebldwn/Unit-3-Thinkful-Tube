73c30b8549310b5ab933b9e1d08b03288a05d0e9
Katie Baldwin <katiebldwn@gmail.com>
2016-11-07 11:24:43 -0600

    Unit 3 Thinkful Tube

---
+++ app.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+$('.searchButton').on('click', function() {
+	var userSearch = $('.search').val();
+	var parameters = {
+		part: "snippet",
+		key: "AIzaSyA4suIzIpF4swPZrnJIdCTLVj27bXfl4x0",
+		q: userSearch,
+		maxResults: 30,
+		r: "json"
+	}
+
+$.getJSON("https://www.googleapis.com/youtube/v3/search", parameters, function(data) {
+	$.each(data.items, function() {
+		if (this.id.kind === 'youtube#video') {
+		var videoLink = "https://www.youtube.com/watch?v="+this.id.videoId;
+		var thumbnail = this.snippet.thumbnails.medium.url;
+		var title = this.snippet.title;
+		$('.results').append("<li class='resultItem'><img src='"+thumbnail+"'><br>" +title+ "</li>");
+		}
+	})
+// $('.results').click(function() {
+// 		$(this).html('<iframe width="560" height="315" src="https://www.youtube.com/embed/'+this.id.videoId+'?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>');
+// 		})
+
+})
+
+})
+
+})
